cdc9118e8528f3fca829f7c1c804f6f9e3120977
Andrey Filchenkov <22079019+AndreyFilchenkov@users.noreply.github.com>
2019-09-21 17:05:50 +0300

    Fix issue 360037

---
+++ src/Sitecore.Support.360037/Sitecore/Buckets/Forms/SelectRenderingDatasourceForm.cs
@@ -0,0 +1,29 @@
+﻿namespace Sitecore.Support.Buckets.Forms
+{
+    using System;
+
+    public class SelectRenderingDatasourceForm : Sitecore.Buckets.Forms.SelectRenderingDatasourceForm
+    {
+        private string CurrentDatasourcePath
+        {
+            get
+            {
+                return ServerProperties["current_datasource"] as string;
+            }
+            set
+            {
+                ServerProperties["current_datasource"] = value;
+            }
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(SelectDatasourceOptions.CurrentDatasource))
+            {
+                CurrentDatasourcePath = SelectDatasourceOptions.CurrentDatasource;
+            }
+
+            base.OnLoad(e);
+        }
+    }
+}
